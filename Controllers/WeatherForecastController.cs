0c0387e5e0d77f1e11df9c561e6b5c4812ca9a3c
Robert <robert.tarczi@gmail.com>
2021-04-18 16:55:34 +0200

    Fixed weather data

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,14 +11,9 @@ namespace api_docker_terraform.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        // private static readonly string[] Summaries = new[]
-        // {
-        //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        // };
-
         private static readonly string[] Summaries = new[]
         {
-            "xxxx", "xxxx", "xxxx", "xxxx", "xxxx", "xxxx", "xxxx", "xxxx", "xxxx", "xxxx"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
